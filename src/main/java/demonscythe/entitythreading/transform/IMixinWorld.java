7044bc7475df569e128fa449d98f6429bde17765
ElectrifiedDed <hardevomail@gmail.com>
2026-03-05 00:56:01 +0300

    123

--- src/main/java/demonscythe/entitythreading/transform/IMixinWorld.java
+++
@@ -1,7 +0,0 @@
-package demonscythe.entitythreading.transform;
-
-import net.minecraft.entity.Entity;
-
-public interface IMixinWorld {
-    void entitythreading$updateChunkPos(Entity entity);
-}
--- src/main/java/demonscythe/entitythreading/transform/mixin/IMixinWorld.java
+++
@@ -1,7 +0,0 @@
-package demonscythe.entitythreading.transform.mixin;
-
-import net.minecraft.entity.Entity;
-
-public interface IMixinWorld {
-    void entitythreading$updateChunkPos(Entity entity);
-}
